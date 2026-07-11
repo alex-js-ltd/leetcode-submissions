cf6a73ffb6dd756f191820ff15229c4b9fd8fb02
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-07-11 11:25:29 +0100

    Time: 360 ms (16.29%) | Memory: 110.1 MB (6.18%) - LeetSync

---
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.ts
@@ -0,0 +1,31 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type ArrayType = { "id": number } & Record<string, JSONValue>;
+
+function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+
+    const seen = new Map();
+
+
+    for(let i = 0; i < arr1.length; i++) {
+
+        if(!seen.has(arr1[i].id)) {
+            seen.set(arr1[i].id, {});
+        }
+
+        seen.set(arr1[i].id, {...arr1[i]});
+    }
+
+    for(let i = 0; i < arr2.length; i++) {
+
+        if(!seen.has(arr2[i].id)) {
+            seen.set(arr2[i].id, {});
+        }
+
+        const prev = seen.get(arr2[i].id);
+        const next = {...prev, ...arr2[i] };
+        seen.set(arr2[i].id, next);
+    }
+    
+
+    return [...seen.values()].sort((a, b) => a.id - b.id);
+};
