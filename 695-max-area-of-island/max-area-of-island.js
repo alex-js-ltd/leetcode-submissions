8b2db6f3be79f182119b98a1c87c4c4d984d5da1
alex-js-ltd <109146897+alex-js-ltd@users.noreply.github.com>
2026-07-14 10:45:57 +0100

    Time: 3 ms (78.53%) | Memory: 55.9 MB (96.08%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+
+    if(grid == null) {
+        return 0;
+    }
+
+    if(grid.length === 0) {
+        return 0;
+    }
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    let maxCount = 0;
+
+    function dfs(r, c) {
+
+        if(r < 0 || r >= rows || c < 0 || c >= cols) {
+            return 0;
+        }
+
+        if(grid[r][c] === 0) {
+            return 0;
+        }
+
+        grid[r][c] = 0;
+
+        return (1 + dfs(r, c + 1) +
+                    dfs(r, c - 1) +
+                    dfs(r + 1, c) +
+                    dfs(r - 1, c)
+        );
+    }
+
+    for(let r = 0; r < rows; r++) {
+
+        for(let c = 0; c < cols; c++) {
+
+            if(grid[r][c] === 1) {
+                const count = dfs(r, c);
+                maxCount = Math.max(maxCount, count);
+            }
+
+        }
+    }
+
+    return maxCount;
+};
